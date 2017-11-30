665302df61f333e06ba51eef4f857e4824c61633
Jackson Stafford <jackson.stafford@utah.edu>
2017-11-30 13:03:35 -0700

    Fix dimension selectors

--- js/dimensions.js
+++ js/dimensions.js
@@ -15,9 +15,9 @@ function drawDimensions(data, limit) {
     let ydim = document.getElementById('ydim').value;
 
 
-    let buffer = 50;
+    let buffer = oneEm * 4//50;
     let width = contentWidth;
-    let height = contentWidth*(2.0/3.0);
+    let height = Math.min(contentWidth*(2.0/3.0), window.innerHeight - 2 * oneEm)
 
     chart.attr("height", height).attr("width", width)
 
@@ -118,6 +118,27 @@ function updateDimensionCharts() {
     })
 }
 
+function makeDimensionSelectors() {
+    let values = 
+        ['acousticness', 'danceability', 'energy', 'instrumentalness', 'liveness', 'popularity', 'speechiness', 'valence']
+    let dim = d3.select('#chart-dim')
+    dim.append('h4').text('Dimensions')
+    dim = dim.selectAll('div').data(['x', 'y']).enter().append('div')
+    dim.append('h5').text(d => d.toUpperCase() + ': ')
+    .append('select')
+        .attr('id', d => d + 'dim')
+        .attr('onchange', 'updateCharts()')
+            .selectAll('option')
+            .data(values)
+            .enter()
+            .append('option')
+            .attr('value', d => d)
+            .text(d => d.toProperCase())
+    dim.select('#xdim')
+        .property('value', values[7])
+    dim.select('#ydim')
+        .property('value', values[1])
+}
 
 function loadDimensions() {
     clearPage();
@@ -134,10 +155,9 @@ function loadDimensions() {
     let dates = ["2016-12-23", "2016-12-30", "2017-01-06", "2017-01-13", "2017-01-20", "2017-01-27", "2017-02-03", "2017-02-10", "2017-02-17", "2017-02-24", "2017-03-03", "2017-03-10", "2017-03-17", "2017-03-24", "2017-03-31", "2017-04-07", "2017-04-14", "2017-04-21", "2017-04-28", "2017-05-05", "2017-05-12", "2017-05-19", "2017-05-26", "2017-06-02", "2017-06-09", "2017-06-16", "2017-06-23", "2017-07-07", "2017-07-14", "2017-07-21", "2017-07-28", "2017-08-11", "2017-08-18", "2017-08-25", "2017-09-01", "2017-09-08", "2017-09-22", "2017-09-29", "2017-10-06", "2017-10-13", "2017-10-20", "2017-10-27", "2017-11-03", "2017-11-10"]
 
 
-
-
-    d3.select('#date-select')
-        .append('select')
+    let date = d3.select('#date-select')
+    date.append('h4').text('Week')
+    date.append('select')
         .attr('id', 'dateSelect')
         .on("change", updateDimensionCharts)
 
@@ -148,27 +168,7 @@ function loadDimensions() {
             .text(date)
     }
 
-    let dims = []
-
-    d3.select('#chart-dim')
-        .append('select')
-        .attr('id', 'xdim')
-        .on("change", updateDimensionCharts)
-        .selectAll('option').data(["valence", "danceability", "energy", "acousticness", "instrumentalness", "liveness", "speechiness"]).enter()
-        .append('option')
-        .attr('value', d => d)
-        .text(d => d)
-
-    d3.select('#chart-dim')
-        .append('select')
-        .attr('id', 'ydim')
-        .on("change", updateDimensionCharts)
-        .selectAll('option').data(["danceability", "valence", "energy", "acousticness", "instrumentalness", "liveness", "speechiness"]).enter()
-        .append('option')
-        .attr('value', d => d)
-        .text(d => d)
-
+    makeDimensionSelectors()
 
     updateDimensionCharts()
 }
-
